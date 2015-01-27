0d825613f2f2605ee6bec18abb7a0d3d72aea285
Matthew Hollings <matthew.hollings@gmail.com>
2015-01-27 19:44:13 +0000

    Add an express server and deps.

---
+++ app.js
@@ -0,0 +1,78 @@
+// require tag
+// set up express
+// static browserify js
+// handlebars render on server and serve
+// socket.io
+// on connect tag.pipe(socket)
+// gulpfile, sass, watchify
+
+var express = require('express'),
+    hbs = require('hbs'),
+    morgan = require('morgan'),
+    logger = require('express-logger'),
+    compression = require('compression'),
+    fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/src/views');
+hbs.registerPartials(__dirname + '/src/views/partials');
+app.use(express.static(__dirname + '/build'));
+
+// Create a write stream for the logs (in append mode)
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: accessLogStream}));
+
+// Gzip the responses
+app.use(compression());
+
+var router = express.Router();
+router.use(function(req, res, next) {
+    // continue doing what we were doing and go to the route
+    next();
+});
+
+// Set up routes
+router.get('/', function(req, res) {
+    var data = {
+        title : 'SensorTag Canvas Drawing',
+        description : 'A small app which send data from a TI SensorTag attached via BLE to the node.js server and draws to an HTML5 canvas element. @mattholl',
+    };
+    res.render('index', data);
+});
+
+// apply the routes to our application
+app.use('/', router);
+
+// Error handlers
+// http://stackoverflow.com/questions/6528876/how-to-redirect-404-errors-to-a-page-in-expressjs
+// Since this is the last non-error-handling
+// middleware use()d, we assume 404, as nothing else
+// responded.
+
+// $ curl http://localhost:3000/notfound
+// $ curl http://localhost:3000/notfound -H "Accept: application/json"
+// $ curl http://localhost:3000/notfound -H "Accept: text/plain"
+
+app.use(function(req, res, next) {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
+app.listen(3000);
+
