f0d9a85ff9f5093b4d2293347e645f5cfd21ed94
Matthew Hollings <matthew.hollings@gmail.com>
2015-01-30 19:09:02 +0000

    Draw loop.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,40 @@
 var ws = require('websocket-stream');
-
 var stream = ws('ws://localhost:3000');
-stream.on('data', function(data) {
-    var tagData = JSON.parse(data.toString());
-    console.log(tagData);
-});
+
+// Read the viewport width and height
+
+
+
+// Global circle position
+var circlePos = {
+    // half the width and height
+};
+
+var frameRate = 60;
+
+// Setup and draw loop
+(function setup() {
+    stream.on('data', function(data) {
+        var tagData = JSON.parse(data.toString());
+        console.log(tagData);
+    });
+    update();
+})();
+
+function update() {
+    setTimeout(function() {
+        requestAnimationFrame(update);
+        draw();
+    }, 1000 / frameRate);
+}
+
+function draw() {
+    // console.log('draw');
+}
+
+
+
+
+
+
+
